93ef9a58e9f8ebf2aa230dd3910acb22bf42e323
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2019-07-13 16:35:25 +0100

    dynamically creating ionic elements

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
 
 confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
@@ -13,5 +14,8 @@ confirmBtn.addEventListener('click', () => {
         return;
     }
 
-    console.log(enteredReason, enteredAmount);
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ": £" + enteredAmount;
+
+    expensesList.appendChild(newItem);
 });
