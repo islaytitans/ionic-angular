1b86fc832fd4e56bf4088e972268d820e2c12d9b
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2019-07-13 16:57:30 +0100

    Added alert functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
 const totalExpensesOutput = document.querySelector('#total-expenses');
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 let totalExpenses = 0;
 
@@ -21,6 +22,13 @@ confirmBtn.addEventListener('click', () => {
     if (enteredReason.trim().length <= 0
         || enteredAmount <= 0
         || enteredAmount.trim().length <= 0) {
+            alertCtrl.create({
+                message: 'Please enter a valid reason and amount',
+                header: 'Invald inputs',
+                buttons: ['Okay']
+            }).then(alertElement => {
+                alertElement.present();
+            });
         return;
     }
 
